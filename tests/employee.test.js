be0e66102a074c222308ab721e49412e9c061b72
Cecy Flores <anacecyflores1@gmail.com>
2022-06-11 20:48:10 -0500

    Updated jest tests with employee.test

---
+++ tests/employee.test.js
@@ -0,0 +1,35 @@
+const Employee = require("../lib/employee.js");
+
+test("generate employee object", () => {
+  const newEmp = new Employee("john", "25", "john@test.com");
+
+  expect(typeof newEmp).toBe("object");
+});
+
+test("generate employee name", () => {
+  const name = "john";
+  const newEmp = new Employee(name);
+
+  expect(newEmp.name).toBe(name);
+});
+
+test("generate employee id", () => {
+  const id = "25";
+  const newEmp = new Employee("john", "25", "john@test.com");
+
+  expect(newEmp.id).toBe(id);
+});
+
+test("generate employee email", () => {
+  const email = "john@test.com";
+  const newEmp = new Employee("john", "25", "john@test.com");
+
+  expect(newEmp.email).toBe(email);
+});
+
+test("generate role", () => {
+  const role = "Employee";
+  const newEmp = new Employee("john", "25", "john@test.com");
+
+  expect(newEmp.getRole()).toBe(role);
+});
